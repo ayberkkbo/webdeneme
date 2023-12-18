361b3e5bd67054c543a5423f6cc3cd9ac9d5fb4b
Jared Galanis <jaredgalanis@users.noreply.github.com>
2023-12-18 14:43:38 -0500

    WIP - rough pass on updating LTS page for data breaking lockstep

--- app/routes/releases/lts.js
+++ app/routes/releases/lts.js
@@ -1,7 +1,11 @@
+import { hash } from 'rsvp';
 import Route from '@ember/routing/route';
 
 export default class ReleasesLtsRoute extends Route {
   model() {
-    return this.store.find('project', 'ember/lts');
+    return hash({
+      ember: this.store.find('project', 'ember/lts'),
+      emberData: this.store.find('project', 'emberData/lts'),
+    });
   }
 }
